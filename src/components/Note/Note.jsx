0a8e3edb8466287a4944def5b04fb7eb9ea13f48
andyhdang <andyhd0614@gmail.com>
2024-09-12 09:15:01 -0700

    added Note folder with html and css

---
+++ src/components/Note/Note.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import Button from '../Button/Button';
+import DoneIcon from '@mui/icons-material/Done';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import './Note.css';
+
+const Note = (props) => {
+    const [isDone, setIsDone] = useState(false);
+
+    function handleDone() {
+        props.onDone(props.id); //used to change isComplete state
+        setIsDone(!isDone); //used to add line-through style
+    }
+
+    function handleEdit() {
+        console.log('Edit clicked');
+        props.onEdit(props.id);
+    }
+
+    return (
+        <div className="note">
+            <div style={{textDecoration: isDone? 'line-through' : null}}className="text-content">
+                <h1>{props.title}</h1>
+                <p>{props.details}</p>
+            </div>
+            <div className='bottom-right'>
+                <div className="button-group">
+                    <Button type='icon' leadingIcon={<EditOutlinedIcon/>} size='small' onClick={handleEdit}></Button>
+                    <Button type='icon' leadingIcon={<DoneIcon/>} size='small' onClick={handleDone}></Button>
+                 </div>
+            
+            </div>
+            
+        </div>
+    );
+};
+
+export default Note;
