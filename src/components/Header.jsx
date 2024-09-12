491c1c85401d28f2596bdafcdab4c67f3cf440df
andyhdang <andyhd0614@gmail.com>
2024-09-12 10:49:11 -0700

    updated header with dynamic day

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
 const Header = () => {
+    const currentDate = new Date();
+    const options = { weekday: "long" };
+    const day = new Intl.DateTimeFormat("en-US", options).format(currentDate);
+
+    const adjectives = ["Great", "Productive", "Awesome", "Fantastic", "Fine", "Good", "Wonderful", "Terrific", "Superb", "Excellent"];
+
+    const randomIndex = Math.floor(Math.random() * adjectives.length);
+    const randomAdjective = adjectives[randomIndex];
+
     return (
         <header>
-            <h1>What do you need to do to consider the day done?</h1>
+            <h1>What do you need to do to consider this <span style={{ color: 'teal' }}>{randomAdjective} {day}</span> done?</h1>
         </header>
     );
 };
